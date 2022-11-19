9dd01b77215d0e60f09abfd5bb0a9ff1cec90076
dearestman <Stupakov.dmitriy1997@gmail.com>
2022-11-19 15:23:28 +0300

    Стартовый проект Spring

---
+++ src/main/java/ru/stupakov/insidemessages/InsideMessagesApplication.java
@@ -0,0 +1,13 @@
+package ru.stupakov.insidemessages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsideMessagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsideMessagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/stupakov/insidemessages/InsideMessagesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.stupakov.insidemessages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsideMessagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
